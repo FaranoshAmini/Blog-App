72a334f8b0d5a29d2bd097261146871da73cfece
Faranosh Amini <faranosh.amini4@gmail.com>
2023-03-31 13:28:50 +0430

    Fix the rubocop errors

--- db/migrate/20230331083034_create_likes.rb
+++ db/migrate/20230331083034_create_likes.rb
@@ -1,9 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
-      
+      t.integer 'author_id'
+      t.integer 'post_id'
+
       t.timestamps
     end
   end
--- db/migrate/20230331083514_create_comments.rb
+++ db/migrate/20230331083514_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.integer 'author_id'
+      t.integer 'post_id'
       t.text :text
 
       t.timestamps
