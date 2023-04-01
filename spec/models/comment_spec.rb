ee5bb8ce33240d312fa9135a572cfeacd81efc6d
Faranosh Amini <faranosh.amini4@gmail.com>
2023-04-02 00:28:53 +0430

    Fix rubocop errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   it 'update_comment_counter should update comments_counter' do
     user = User.create(name: 'Faranosh', posts_counter: 0)
     post = Post.create(author: user, title: 'First Post', likes_counter: 0)
-    Comment.create(author: user, post: post, text: 'first Comment!')
+    Comment.create(author: user, post:, text: 'first Comment!')
     expect(post.comments_counter).to eq(0)
   end
 end
